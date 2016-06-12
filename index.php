ecff8359f4541d2d0d07b2ffa8019b337d85001f
JosephKot <joseph@codemode.ca>
2016-06-11 22:11:34 -0700

    Setting up the project
    Building index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+
+			<!-- Goto Bootstrap, jQuery, jQuery UI and FOntAwesome and
+			 copy/paste the CDNs into the head -->
+
+			<!-- Latest compiled and minified Bootstrap CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+			<!-- Optional Bootstrap theme -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+			<!-- jQuery UI CSS CDN -->
+			<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+
+			<!-- Fontawesome -->
+			<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+			<!-- jQuery CDN -->
+			<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+
+			<!-- jquery UI CDN-->
+			<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+
+			<!-- Latest compiled and minified Bootstrap JavaScript -->
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+	</head>
+
+	<body>
+		
+		<h1><i class="fa fa-leaf fa-5x"></i>codemode.ca</h1><!-- A quick test to see if the head is calling everything -->
+
+	</body>
+</html>
